3402f837bf89d004384a607f2a9be4584f17a4da
BounceyQueso <sheetz.casey@gmail.com>
2020-04-24 17:42:24 -0700

    chapter 3 & 4! Love it <3

---
+++ FreeNove_Projects/Chapter4_Breathing_LED.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 12
+
+def setup():
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.setup(ledPin, GPIO.LOW)
+    
+    p = GPIO.PWM(ledPin, 200)
+    p.start(0)
+
+def loop():
+    while True:
+        for dc in range(0, 101, 1):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+        time.sleep(.5)
+        for dc in range(100,-1,-1):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+            
+def destroy():
+    GPIO.cleanup()
+    
+if __name__ == '__main__':
+    print ('Program is starting... \n')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
