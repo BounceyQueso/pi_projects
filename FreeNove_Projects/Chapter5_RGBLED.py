0b402e9ed5ff824ef214628a7eb52e03fc5ee67b
BounceyQueso <sheetz.casey@gmail.com>
2020-05-04 19:13:22 -0700

    fixed chapter5, working version

--- FreeNove_Projects/Chapter5_RGBLED.py
+++ FreeNove_Projects/Chapter5_RGBLED.py
@@ -2,35 +2,40 @@ import RPi.GPIO as GPIO
 import time
 import random
 
-pins = {'redPin':29, 'greenPin':12, 'bluePin':32}
-pin_list = list(pins.values())
+redPin = 11
+greenPin = 12
+bluePin = 13
 
 def setup():
-    global pwmRed, pwmGreen, pwmBlue
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin_list, GPIO.OUT)
-    GPIO.setup(pin_list, GPIO.HIGH)
-    pwmRed = GPIO.PWM(pins['redPin'], 2000)
+    GPIO.setup(redPin, GPIO.OUT, initial=1)
+    GPIO.setup(greenPin, GPIO.OUT, initial=1)
+    GPIO.setup(bluePin, GPIO.OUT, initial=1)
+    
+def startPWM():
+    global pwmRed, pwmGreen, pwmBlue
+    pwmRed = GPIO.PWM(redPin, 2000)
+    pwmGreen = GPIO.PWM(greenPin, 2000)
+    pwmBlue = GPIO.PWM(bluePin, 2000)
+    
     pwmRed.start(0)
-    pwmGreen = GPIO.PWM(pins['greenPin'], 2000)
     pwmGreen.start(0)
-    pwmBlue = GPIO.PWM(pins['bluePin'], 2000)
     pwmBlue.start(0)
     
-def setColor(r_val, g_val, b_val):
+def setColor(r_val,g_val,b_val):
     pwmRed.ChangeDutyCycle(r_val)
     pwmGreen.ChangeDutyCycle(g_val)
     pwmBlue.ChangeDutyCycle(b_val)
-
+    
 def loop():
     while True:
         r=random.randint(0,100)
         g=random.randint(0,100)
         b=random.randint(0,100)
         setColor(r,g,b)
-        print ('r=%d, g=%d, b=%d' %(r,g,b))
+        print('r=%d, g=%d, b=%d' %(r,g,b))
         time.sleep(0.3)
-            
+        
 def destroy():
     pwmRed.stop()
     pwmGreen.stop()
@@ -38,8 +43,9 @@ def destroy():
     GPIO.cleanup()
     
 if __name__ == '__main__':
-    print ('Program is starting... \n')
+    print('Program is starting...')
     setup()
+    startPWM()
     try:
         loop()
     except KeyboardInterrupt:
